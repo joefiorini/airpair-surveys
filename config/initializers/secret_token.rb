4ff7c4d76fa7f9a5ccb7fa9488ca0b2cc3ce1785
Joe Fiorini <joe@joefiorini.com>
2014-03-20 20:49:24 -0400

    Setup models and ember-appkit-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Airpair::Application.config.secret_key_base = 'add7a1bc672409c5ffb4d1647c5011704a9b67cf3f54163984459b338a5171b04ddd2ae488f4c90caa1caba5da325e5dc39b484a5e847ccb6943af81dfda8683'
